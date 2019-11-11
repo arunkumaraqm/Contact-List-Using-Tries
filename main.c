dbee76481c0907ced245b6d2cb3808e898fd1e6a
Arun Kumar <arunkumar.aqm@gmail.com>
2019-11-11 11:48:32 +0530

    Change variable names and add t_ functions

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+// Contacts List (Names and Phone Numbers) using Tries
+// Contributors:
+// Arun, Anirudh BM, Anirudh Shastri, Ayaan, Amogh
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,26 +10,44 @@
 
 // Alphabet size (Number of symbols)
 #define ALPHABET_SIZE (26)
+#define MAX_NAME_LEN (20)
 
 // Converts key current character into index
 // use only 'a' through 'z' and lower case
 #define CHAR_TO_INDEX(c) ((int)c - (int)'a')
 
-// trie node
-struct TrieNode
+char *strlwr(char *str) // Remove this if you get a compilation error
 {
-    struct TrieNode *children[ALPHABET_SIZE];
+  unsigned char *p = (unsigned char *)str;
+
+  while (*p) {
+     *p = tolower((unsigned char)*p);
+      p++;
+  }
+
+  return str;
+}
+
+// trie node
+typedef struct Node
+{
+    struct Node *children[ALPHABET_SIZE];
 
     // isLeaf is true if the node represent end of a word.
     bool isLeaf;
-};
+} Node;
 
-typedef struct TrieNode* NODEPTR;
+typedef struct Node* NodePointer;
+
+typedef struct Trie{
+
+	NodePointer root;
+} Trie;
 
 // Returns new trie node (initialized to NULLs)
-NODEPTR getNode(void)
+NodePointer create_node(void)
 {
-    NODEPTR newNode = (NODEPTR)malloc(sizeof(struct TrieNode));
+    NodePointer newNode = (NodePointer)malloc(sizeof(struct Node));
 
     if (newNode!=NULL)
     {
@@ -36,15 +58,15 @@ NODEPTR getNode(void)
     }
     else
     {
-            printf("\n\nFailed to create the Node... Malloc Failed...\n");
+            printf("Heap Full.\n");
     }
     return newNode;
 }
 
 // Inserts a given word to dictionary.
-void insert(NODEPTR root, const char *key)
+void n_add_contact(NodePointer root, const char *key)
 {
-    NODEPTR temp = root;
+    NodePointer temp = root;
 
     int i;
     for (i = 0; i < strlen(key); i++)
@@ -54,7 +76,7 @@ void insert(NODEPTR root, const char *key)
 
         // Checks whether node is already present, if not then create one
         if (temp->children[index]==NULL)
-            temp->children[index] = getNode();
+            temp->children[index] = create_node();
 
         // To traverse to next node.
         temp = temp->children[index];
@@ -65,9 +87,9 @@ void insert(NODEPTR root, const char *key)
 }
 
 // Returns true if key presents in the dictionary, else returns false
-bool search( NODEPTR root, const char *key)
+bool n_search_contact( NodePointer root, const char *key)
 {
-    NODEPTR temp = root;
+    NodePointer temp = root;
 
     int i;
     for (i = 0; i < strlen(key); i++)
@@ -84,9 +106,9 @@ bool search( NODEPTR root, const char *key)
 }
 
 // Used for prefix search to get the root.
-NODEPTR traverseTill(NODEPTR root, const char *key)
+NodePointer n_traverse_till(NodePointer root, const char *key)
 {
-    NODEPTR temp = root;
+    NodePointer temp = root;
 
     int i;
     for (i = 0; i < strlen(key); i++)
@@ -103,10 +125,10 @@ NODEPTR traverseTill(NODEPTR root, const char *key)
 }
 
 // To check the words are having only alphabets and converts to lower case.
-char* validation(char* input){
+char* is_valid(char* input){
 
     int i;
-    for(i=0;i<strlen(input);i++){
+    for(i=0;input[i];i++){
         if(!isalpha(input[i])){
             printf("\n\nNot a valid Word !!!\n\n");
             return NULL;
@@ -116,7 +138,7 @@ char* validation(char* input){
 }
 
 // To display all words present in the dictionary.
-void printTrieContents(NODEPTR root, char *buffer, int buffIndex)
+void n_display_all_contacts(NodePointer root, char *buffer, int buffIndex)
 {
     bool flag=true;
     //bool emptyDictionary=true;
@@ -140,8 +162,9 @@ void printTrieContents(NODEPTR root, char *buffer, int buffIndex)
         if(root->children[i] != NULL)
         {
                 buffer[buffIndex] = i + 'a';
-                printTrieContents(root->children[i], buffer, buffIndex + 1);
+                n_display_all_contacts(root->children[i], buffer, buffIndex + 1);
         }
+        
         if(root->isLeaf && flag)
         {
                 buffer[buffIndex] = '\0';
@@ -154,80 +177,109 @@ void printTrieContents(NODEPTR root, char *buffer, int buffIndex)
     }
 }
 
+void t_constructor(Trie* trie){
+
+	trie->root = create_node();	
+}
+
+void t_add_contact(Trie* trie){
+
+	char new_contact_name[MAX_NAME_LEN];
+	scanf("%s", new_contact_name);
+	
+    if (is_valid(new_contact_name))
+    	n_add_contact(trie->root, strlwr(new_contact_name));
+    	
+    else
+    	printf("Name does not meet validity criteria.\n");
+}
+
+void t_search_prefix(Trie* trie){
+
+	char prefix[MAX_NAME_LEN];
+	scanf("%s", prefix);
+               
+    if (is_valid(prefix)){
+    
+    	char buffer[MAX_NAME_LEN];
+    	strcpy(buffer, prefix);
+    	
+    	NodePointer prefix_root = n_traverse_till(trie->root, prefix);
+
+    	if (prefix_root != NULL){
+    	
+    		n_display_all_contacts(prefix_root, buffer, strlen(prefix));
+		}
+	}        
+}
+
+void t_search_contact(Trie* trie){
+
+	char item[MAX_NAME_LEN];
+	int yn;
+	scanf("%s", item);
+	
+	if (is_valid(item)){
+
+		if (n_search_contact(trie->root,item))
+			printf("\n%s is present in the dictionary.\n", item);
+		
+		else{
+
+			printf("\n%s is not present in the dictionary.\n", item);
+
+			printf("\nShall I add it ? (Yes - 1 or No - 0):");
+			scanf("%d", &yn);
+			
+			if (yn == 1)
+				n_add_contact(trie->root, item);
+
+		}
+	}
+}
+
+void t_display_all_contacts(Trie* trie){
+
+	char buffer[MAX_NAME_LEN];
+	n_display_all_contacts(trie->root, buffer, 0);
+}
+
 int main()
 {
-    char buffer[20];
+	Trie trie;
+	t_constructor(&trie);
+	
+	int option;
 
-    NODEPTR root = getNode();
-
-    printf(".......Welcome to the Data Structure Project on Dictionary Implementation Using TRIE........\n");
-
-    int option ;
-    char wordToDictionary[20];
-    char wordToSearch[20];
-    char prefix[20];
-    char yesOrNo[10];
-    //infinite loop
-    while(1)
+    do
     {
-        printf("\n1) ADD the Word to the dictionary.\n");
-        printf("2) PRINT_ALL the words in the dictionary.\n");
-        printf("3) SEARCH a word from the dictionary.\n");
-        printf("4) PRINT_ALL words with a given PRIFIX.\n");
-        printf("5) EXIT from the menu.\n");
-        printf("Select your option : ");
         scanf("%d",&option);
 
         switch(option)
         {
-        case 1:printf("\nEnter the word to add to dictionary : ");
-               scanf("%s", wordToDictionary);
-               if(validation(wordToDictionary)!=NULL)
-                   insert(root, strlwr(wordToDictionary));
-               break;
-
-        case 2:printf("\nContents in the Dictionary are:\n");
-               printTrieContents(root,buffer,0);
-               break;
-
-        case 3:printf("\nEnter the word to SEARCH : ");
-               scanf("%s",wordToSearch);
-               if(validation(wordToSearch)!=NULL)
-                {
-                    if(search(root,wordToSearch))
-                        printf("\n%s is present in the dictionary.\n",wordToSearch);
-                else
-                {
-                    printf("\n%s is not present in the dictionary.\n",wordToSearch);
-
-                    printf("\nShall I add it ? (Yes or No):");
-                    scanf("%s",yesOrNo);
-                    if(strcmp("yes",yesOrNo)==0||strcmp("YES",yesOrNo)==0||strcmp("Yes",yesOrNo)==0)
-                            insert(root, wordToSearch);
-
-                }
-                }
-                break;
-
-        case 4:printf("\nEnter the PRIFIX : ");
-               scanf("%s",prefix);
-               if(validation(prefix)!=NULL)
-               {
-                    strcpy(buffer,prefix);
-                    printf("\nPrinting all the dictionary words with given PRIFIX : %s \n",prefix);
-                    NODEPTR prefixRoot= traverseTill(root,prefix);
-                    if(prefixRoot!=NULL)
-                        {
-                            printTrieContents(prefixRoot,buffer,strlen(prefix));
-                        }
-                }
-                break;
-
-        case 5:printf("\n\nTHANK YOU !!! :) \n\n");
-               exit(0);
-
-        default:printf("\n\nInvalid Option.\n\n");
+        case 1: t_add_contact(&trie); break;
+        
+        case 2: t_search_prefix(&trie); break;
+        
+        case 3: t_search_contact(&trie); break;
+        
+        case 4: t_display_all_contacts(&trie); break;
+        
+        default: option = 0;
+        
         }
-    }
+        
+    }while(option);
+    
     return 0;
 }
+/*
+1 Adam
+1 Angelo
+1 Ada
+1 Addison
+1 Bob
+1 Bojack
+1 Carmela
+1 Chad
+*/
